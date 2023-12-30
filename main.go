29035815d6c3ae9ecadc4cfd403e1b0b5f0a9923
unsubstantiated-Script <jusfulton@gmail.com>
2023-12-29 21:18:19 -0800

    aind again

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package goSchool
+package main
+
+import (
+	"fmt"
+	"github.com/unsubstantiated-Script/goDawggy"
+)
+
+func main() {
+	s1 := goDawggy.Bark()
+	s2 := goDawggy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
