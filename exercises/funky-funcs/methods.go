d24335b19da14c477a11765bb94d886fb4fdc779
unsubstantiated-Script <jusfulton@gmail.com>
2024-01-13 08:32:35 -0800

    interface setup

--- exercises/funky-funcs/methods.go
+++ exercises/funky-funcs/methods.go
@@ -6,9 +6,42 @@ type person struct {
 	first string
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+// This interface allows polymorphism. Hey baby, if you've got these methods, then you're my type ;X
+type human interface {
+	speak()
+}
+
 // Here is how the method plays out and the receiver binds it to the struct above.
 func (p person) speak() {
 	fmt.Println("I am", p.first)
+}
+
+// Here is how the method plays out and the receiver binds it to the struct above.
+func (sa secretAgent) speak() {
+	ltk := determineLTK(sa.ltk)
+	fmt.Printf("I am %v a secret agent man. I %v have a license to kill.\n", sa.person.first, ltk)
+}
+
+func determineLTK(ltkBool bool) string {
+	ltk := ""
+
+	if ltkBool {
+		ltk = "do"
+	} else {
+		ltk = "do not"
+	}
+
+	return ltk
+}
+
+// interfacing with the interface
+func saySomething(h human) {
+	h.speak()
 }
 
 func RollMethods() {
@@ -20,6 +53,29 @@ func RollMethods() {
 		first: "Jimbo",
 	}
 
-	p1.speak()
-	p2.speak()
+	//p1.speak()
+	//p2.speak()
+
+	saySomething(p1)
+	saySomething(p2)
+
+	sa1 := secretAgent{
+		person: person{
+			first: "Skippy",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			first: "Danger",
+		},
+		ltk: false,
+	}
+
+	//sa1.speak()
+	//sa2.speak()
+
+	saySomething(sa1)
+	saySomething(sa2)
 }
