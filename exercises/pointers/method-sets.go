9310e1b5ed7c8b5617b6f1c82cb5635843183f80
unsubstantiated-Script <jusfulton@gmail.com>
2024-01-19 19:19:08 -0800

    pointers done

--- exercises/pointers/method-sets.go
+++ exercises/pointers/method-sets.go
@@ -18,14 +18,25 @@ func (d *dog) run() {
 	fmt.Println("My name is", d.first, "and I'm running.")
 }
 
+type youngin interface {
+	walk()
+	run()
+}
+
+func youngRun(y youngin) {
+	y.run()
+}
 func RollMethodSets() {
 
 	//These will show that method calls will still where here on method sets.
 	d1 := dog{"Henry"}
 	d1.walk()
 	d1.run()
+	youngRun(&d1)
 
 	d2 := &dog{"Deetz"}
 	d2.walk()
 	d2.run()
+	youngRun(d2)
+
 }
